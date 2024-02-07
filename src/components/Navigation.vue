336c8c0252dd6145bb9197aca78047d934f95a3d
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2024-02-07 22:34:39 +0700

    create a component for navigation and sidebar

---
+++ src/components/Navigation.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="px-2 sm:px-6 lg:px-8 shadow-md">
+    <div class="flex h-16 items-center">
+      <span class="font-inter font-bold">
+        Conversion
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="h-screen">
+    <!-- Sidebar -->
+    <aside class="bg-white text-gray-800 w-64 flex-shrink-0 shadow-md h-full">
+      <!-- Sidebar Content -->
+      <div class="p-4">
+        <!-- Logo -->
+        <div class="text-3xl font-inter font-extrabold">
+          heylink.
+        </div>
+
+        <!-- Sidebar Links -->
+        <nav class="mt-4">
+          <router-link
+            to="/"
+            class="text-xl block font-medium py-2 px-4 text-gray-800 hover:bg-gray-200 hover:rounded-[8px] transition duration-300 flex items-center"
+            :class="{ 'bg-[#E3F2FD] rounded-[8px]': $route.path === '/' }"
+          >
+            <Icon icon="solar:graph-down-outline" class="mr-2" />
+            Home
+          </router-link>
+          <router-link
+            to="/overview"
+            class="text-xl block font-medium py-2 px-4 text-gray-800 hover:bg-gray-200 hover:rounded-[8px] transition duration-300 flex items-center"
+            :class="{ 'bg-[#E3F2FD] rounded-[8px]': $route.path === '/overview' }"
+          >
+            <Icon icon="solar:graph-up-outline" class="mr-2" />
+            Overview
+          </router-link>
+        </nav>
+      </div>
+      <!-- End Sidebar Content -->
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+}
+</script>
+
+<style scoped>
+/* Add your custom styles here */
+</style>
