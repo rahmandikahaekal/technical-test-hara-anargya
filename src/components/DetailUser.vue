711118a423e50f1148d0df23874f00dfa395ea38
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2024-02-08 01:17:23 +0700

    re-useable component for detail user modal popup

---
+++ src/components/DetailUser.vue
@@ -0,0 +1,163 @@
+<template>
+  <transition name="fade">
+    <div v-if="isVisible" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+      <div class="bg-white p-8 rounded shadow-md relative">
+        <button @click="closeModal" class="bg-red-500 hover:bg-red-700 transition duration-300 text-white rounded p-2 absolute right-5 top-5">
+          <Icon icon="solar:close-square-linear" class="text-2xl" />
+        </button>
+
+        <div class="flex justify-center mb-2">
+          <img :src="selectedUser.profile_picture" class="w-20 h-20 object-cover rounded-full mr-4" />
+          <div>
+            <h2 class="text-2xl font-bold mb-4">
+              {{ setNama(selectedUser) }}
+            </h2>
+            <p>{{ selectedUser.email }}</p>
+            <p>{{ selectedUser.phone }}</p>
+          </div>  
+        </div>
+
+        <div class="flex flex-row">
+          <div class="basis-2/5 flex flex-col pr-2">
+            <div class="flex justify-between">
+              <span>
+                Date Of Birth
+              </span>
+              <span>
+                :
+              </span>
+            </div>
+            <div class="flex justify-between">
+              <span>
+                Gender
+              </span>
+              <span>
+                :
+              </span>
+            </div>
+            <div class="flex justify-between">
+              <span>
+                Job
+              </span>
+              <span>
+                :
+              </span>
+            </div>
+            <div class="flex justify-between">
+              <span>
+                Address
+              </span>
+              <span>
+                :
+              </span>
+            </div>
+            
+            <div class="flex justify-between">
+              <span>
+                City
+              </span>
+              <span>
+                :
+              </span>
+            </div>
+            <div class="flex justify-between">
+              <span>
+                State
+              </span>
+              <span>
+                :
+              </span>
+            </div>
+            <div class="flex justify-between">
+              <span>
+                Country
+              </span>
+              <span>
+                :
+              </span>
+            </div>
+          </div>
+          <div class="basis-3/5">
+            <div>
+              {{ formatDate(selectedUser.date_of_birth) }}
+            </div>
+            <div>
+              {{ selectedUser.gender }}
+            </div>
+            <div>
+              {{ selectedUser.job }}
+            </div>
+            <div>
+              {{ selectedUser.street }}
+            </div>
+            <div>
+              {{ selectedUser.city }}, {{ selectedUser.zipcode }}
+            </div>
+            <div>
+              {{ selectedUser.state }}
+            </div>
+            <div>
+              {{ selectedUser.country }}
+            </div>
+          </div>
+        </div>
+
+        <!-- <div class="mt-4">
+          <iframe
+            width="100%"
+            height="300"
+            :src="getGoogleMapsEmbedUrl(selectedUser.latitude, selectedUser.longitude)"
+            frameborder="0"
+            style="border:0"
+            allowfullscreen=""
+            loading="lazy"
+          ></iframe>
+        </div> -->
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    isVisible: {
+      type: Boolean,
+    },
+    selectedUser: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  methods: {
+    setNama(data) {
+      const first_name = data.first_name;
+      const last_name = data.last_name;
+      return first_name + ' ' + last_name;
+    },
+    formatDate(dateString) {
+      const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+      const formattedDate = new Date(dateString).toLocaleDateString(undefined, options);
+      return formattedDate;
+    },
+    closeModal() {
+      this.$emit('close');
+    },
+    // getGoogleMapsEmbedUrl(latitude, longitude) {
+    //   const apiKey = 'YOUR_GOOGLE_MAPS_API_KEY'; 
+    //   const embedUrl = `https://www.google.com/maps/embed/v1/view?key=${apiKey}&center=${latitude},${longitude}&zoom=15`;
+    //   return embedUrl;
+    // },
+  },
+};
+</script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
