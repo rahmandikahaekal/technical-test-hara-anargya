19eb6a7f2eafbd914a23f1e38cb5ab94596343ae
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2024-02-07 22:34:23 +0700

    build a structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
-<script setup></script>
-
 <template>
-  <div class="text-lg font-bold">Hallo Folks!</div>
+  <div class="flex h-screen" id="app">
+    <Sidebar />
+    <div class="flex flex-col flex-1">
+      <Navigation />
+      <div class="bg-[#f7f8fa] flex-1 overflow-hidden">
+        <RouterView />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+import { RouterView } from 'vue-router';
+import Sidebar from './components/Sidebar.vue';
+import Navigation from './components/Navigation.vue';
+
+export default {
+  components: {
+    Sidebar,
+    Navigation
+  }
+};
+</script>
+
+<style scoped>
+/* Gaya CSS sesuai kebutuhan */
+</style>
