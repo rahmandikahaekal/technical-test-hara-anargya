2f50578f463a901a9e58667e835765de9019971d
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2024-02-07 22:33:21 +0700

    create content wrap of home and overview for body dashboard

---
+++ src/views/HomeView.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="container mx-auto px-8 mt-8">
+    <div class="grid gap-x-8 gap-y-4 grid-cols-3">
+      <div class="card bg-white border-gray-700 shadow-md rounded-[10px]">
+        <div class="p-4 flex justify-between">
+          <div>
+            <span class="text-xs font-bold text-slate-500">Pending</span>
+            <p class="text-2xl text-md font-bold pt-2">
+              2.480,
+              <span class="text-slate-400">
+                30
+              </span>
+            </p>
+          </div>
+          <div class="bg-[#f2f4f7] p-2 h-fit rounded">
+            <Icon icon="solar:square-transfer-horizontal-broken" class="text-2xl text-slate-600" />
+          </div>
+        </div>
+        <div class="py-4 px-5 flex justify-between bg-slate-100 rounded-b-lg flex items-center">
+          <div class="flex items-center">
+            <span class="font-semibold text-xs flex items-center text-green-600 mr-2">
+              <Icon icon="solar:arrow-right-up-outline" class="text-xl mr-1" />
+              2,15%
+            </span>
+            <span class="text-slate-400 text-xs">
+              Last Month
+            </span>
+          </div>
+          <div class="text-slate-500 text-xs font-semibold">
+            <button>
+              View More
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card bg-white border-gray-700 shadow-md rounded-[10px]">
+        <div class="p-4 flex justify-between">
+          <div>
+            <span class="text-xs font-bold text-slate-500">Paid</span>
+            <p class="text-2xl text-md font-bold pt-2">
+              84.310
+            </p>
+          </div>
+          <div class="bg-[#f2f4f7] p-2 h-fit rounded">
+            <Icon icon="solar:square-transfer-horizontal-broken" class="text-2xl text-slate-600" />
+          </div>
+        </div>
+        <div class="py-4 px-5 flex justify-between bg-slate-100 rounded-b-lg flex items-center">
+          <div class="flex items-center">
+            <span class="font-semibold text-xs flex items-center text-red-600 mr-2">
+              <Icon icon="solar:arrow-right-down-outline" class="text-xl mr-1" />
+              1,10%
+            </span>
+            <span class="text-slate-400 text-xs">
+              Last Month
+            </span>
+          </div>
+          <div class="text-slate-500 text-xs font-semibold">
+            <button>
+              View More
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card bg-white border-gray-700 shadow-md rounded-[10px]">
+        <div class="p-4 flex justify-between">
+          <div>
+            <span class="text-xs font-bold text-slate-500">Rejected</span>
+            <p class="text-2xl text-md font-bold pt-2">
+              12.155
+            </p>
+          </div>
+          <div class="bg-[#f2f4f7] p-2 h-fit rounded">
+            <Icon icon="solar:square-transfer-horizontal-broken" class="text-2xl text-slate-600" />
+          </div>
+        </div>
+        <div class="py-4 px-5 flex justify-between bg-slate-100 rounded-b-lg flex items-center">
+          <div class="flex items-center">
+            <span class="font-semibold text-xs flex items-center text-green-600 mr-2">
+              <Icon icon="solar:arrow-right-up-outline" class="text-xl mr-1" />
+              2,15%
+            </span>
+            <span class="text-slate-400 text-xs">
+              Last Month
+            </span>
+          </div>
+          <div class="text-slate-500 text-xs font-semibold">
+            <button>
+              View More
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-row mt-8">
+      test
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
