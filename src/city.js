c5769596b02b516c82afa30f9547e7c04f9a66c1
rahmandikahaekal <rahmandikahaekal31@gmail.com>
2024-02-08 17:15:32 +0700

    adding city.js

---
+++ src/city.js
@@ -0,0 +1,19 @@
+
+const cities = {
+  1: [
+    { id: 101, name: 'Banda Aceh' },
+    { id: 102, name: 'Langsa' },
+    { id: 103, name: 'Sabang' },
+  ],
+  2: [
+    { id: 201, name: 'Denpasar' },
+    { id: 202, name: 'Kuta' },
+    { id: 203, name: 'Ubud' },
+  ],
+  3: [
+    { id: 301, name: 'Pangkalpinang' },
+    { id: 302, name: 'Tanjungpandan' },
+  ],
+};
+
+export default cities;
